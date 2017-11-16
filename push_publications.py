54f0befd44e22b9863c4878db6a92b6198487edb
bbm <brian.maranville@nist.gov>
2017-11-16 16:57:03 -0500

    moving push into class, so that connect is not called on import with paramiko (caused thread race condition block)

--- push_publications.py
+++ push_publications.py
@@ -1,29 +1,27 @@
 import paramiko
 
-DEBUG = False
-RETRIEVE_METHOD = "ssh" # or "ftp" or "urllib"
-MAX_FTP_RETRIES = 5
-HOST_PORT = 22
+DEST_HOST = "webster.ncnr.nist.gov"
+DEST_PORT = 22
+DEST_PKEY_FILE = '/home/bbm/.ssh/datapushkey'
+DEST_USERNAME = "bbm"
 
-dest_host = "webster.ncnr.nist.gov"                    #hard-coded
-dest_port = 22
+class SFTPConnection(object):
+    def __init__(self, dest_host=DEST_HOST, dest_port=DEST_PORT):
+        self.transport = paramiko.Transport((dest_host, dest_port))
+        self.sftp = None
+        
+    def connect(self, username=DEST_USERNAME, pkey_file=DEST_PKEY_FILE, pkey_passphrase=None):
+        pkey = paramiko.RSAKey(filename=pkey_file, password=pkey_passphrase)
+        self.transport.connect(username=username, pkey=pkey)
+        self.transport.window_size = 2147483647
+        self.transport.use_compression(True)
+        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        
+    def push_item(self, path, data, make_temporary=True):
+        dest_path = path if not make_temporary else path + ".tmp"
+        f = self.sftp.open(dest_path, 'w')
+        f.write(data)
+        f.close()
+        if make_temporary:
+            self.sftp.posix_rename(dest_path, path)
 
-# I have a different key for pushing to webster.
-# TODO: move these to command line arguments.
-dest_pkey = paramiko.RSAKey(filename='/home/bbm/.ssh/datapushkey')
-dest_username = "bbm"
-
-# Now initialize the transfer to the destination:    
-dest_transport = paramiko.Transport((dest_host, dest_port))
-dest_transport.connect(username = dest_username, pkey = dest_pkey)
-dest_transport.window_size = 2147483647
-dest_transport.use_compression(True)
-dest_sftp = paramiko.SFTPClient.from_transport(dest_transport)
-
-def push_item(path, data, make_temporary=True):
-    dest_path = path if not make_temporary else path + ".tmp"
-    f = dest_sftp.open(dest_path, 'w')
-    f.write(data)
-    f.close()
-    if make_temporary:
-        dest_sftp.rename(dest_path, path)
