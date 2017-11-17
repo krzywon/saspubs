164f18949292b85f6e1d2fd66131c811ed586e1c
bbm <brian.maranville@nist.gov>
2017-11-17 11:40:54 -0500

    don't open server sftp connection unless moving a file

--- push_publications.py
+++ push_publications.py
@@ -1,9 +1,10 @@
 import json
 import os
 
+from server_sftp import SFTPConnection
 from config import INSTRUMENTS, DB_PATH, DB_FILENAME_FMT, VERSION_FILENAME_FMT, REMOTE_PATH
 
-def push_instrument(instrument, server_connection):
+def push_instrument(instrument):
     db_filename = DB_FILENAME_FMT.format(instrument=instrument)
     db_fullpath = os.path.join(DB_PATH, db_filename)
     version_filename = VERSION_FILENAME_FMT.format(db_path=DB_PATH, instrument=instrument)
@@ -19,15 +20,14 @@ def push_instrument(instrument, server_connection):
         new_mtime = os.stat(db_fullpath).st_mtime
         old_mtime = version_info.get("file_mtime", 0)
         if new_mtime > old_mtime:
+            server_connection = SFTPConnection()
+            server_connection.connect()
             server_connection.sftp.put(db_fullpath, REMOTE_PATH + 'data/' + db_filename)
             version_info["file_mtime"] = new_mtime
             open(version_fullpath, "w").write(json.dumps(version_info, 2))
             
 if __name__=='__main__':
     import sys
-    from server_sftp import SFTPConnection
-    server_connection = SFTPConnection()
-    server_connection.connect()
     if len(sys.argv) > 1:
         push_instrument(sys.argv[1], server_connection)
         
