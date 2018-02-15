a8ae59430152b022ab641cc8edaf4e857b886285
krzywon <krzywon@yahoo.com>
2018-02-15 15:13:21 -0500

    Added a page for all SANS pages based off the SANS collection. Added unique headers for individual SANS pages.

--- config.py
+++ config.py
@@ -1,4 +1,54 @@
-SANS_HEADER = r"""\
+NG7SANS_HEADER = r"""
+<p>If you have written a paper that reports measurements using the NG7 30m SANS Instrument and you do not see it in the list below, or if the paper is listed but the 
+information is incorrect or out of date, please inform <a href="mailto:jkrzywon@nist.gov">Jeff Krzywon</a> and/or <a href="mailto:butler@nist.gov">Paul Butler</a>.</p>
+<p>If you write a paper that reports measurements using the NG7 30m SANS Instrument, please be sure to
+	<ol>
+		<li>include an acknowledgement to NIST if there are no NIST coauthors,and</li>
+    <li>include a reference to the paper describing the 30m SANS Instruments: "<a href="http://dx.doi.org/10.1107/S0021889897017020">The 30 m Small-Angle Neutron Scattering Instruments at the National Institute of Standards and Technology</a>", Glinka CJ, et al. <i>J Appl. Cryst.</i> <b>31</b>(3), 430 (1998).  Thanks!</li>
+  </ol>
+<p>If you reduced your data using the Igor Macros, please include the reference ""<a href="http://dx.doi.org/10.1107/S0021889806035059">Reduction and Analysis of SANS and USANS Data using Igor Pro</a>", Kline, S. R. <i>J Appl. Cryst.</i> <b>39</b>(6), 895 (2006).</p>
+<p>Please be aware that all papers with NIST coauthors must be reviewed and approved by the Washington Editorial Review Board (WERB). Members of WERB are concerned not only with scientific merit but with three specific matters: measurement uncertainties, the use of SI units, and brand names. For more detailed information please <A href="https://www.ncnr.nist.gov/WERB.html">read more here</a> and/or speak with your NIST coauthor(s).</p>
+"""
+
+NGB30SANS_HEADER = r"""
+<p>If you have written a paper that reports measurements using the NGB 30m or NG3 30 SANS Instrument and you do not see it in the list below, or if the paper is listed but the 
+information is incorrect or out of date, please inform <a href="mailto:jkrzywon@nist.gov">Jeff Krzywon</a> and/or <a href="mailto:butler@nist.gov">Paul Butler</a>.</p>
+<p>If you write a paper or give a presentation that reports measurements using the NG3/NGB30m SANS Instrument, please be sure to:</p>
+<ol>
+<li>include an <A href="/programs/CHRNS/publications.html">acknowledgement</A> to the NSF, and to NIST if there are no NIST coauthors,and</li>
+<li>include a reference to the paper describing the 30m SANS Instruments: "<a href="http://dx.doi.org/10.1107/S0021889897017020">The 30 m Small-Angle Neutron Scattering Instruments at the National Institute of Standards and Technology</a>", Glinka CJ, et al. <i>J Appl. Cryst.</i> <b>31</b>(3), 430 (1998).  Thanks!</li>
+</ol>
+<p>If you reduced your data using the Igor Macros, please include the reference ""<a href="http://dx.doi.org/10.1107/S0021889806035059">Reduction and Analysis of SANS and USANS Data using Igor Pro</a>", Kline, S. R. <i>J Appl. Cryst.</i> <b>39</b>(6), 895 (2006).</p>
+<p>Please be aware that all papers with NIST coauthors must be reviewed and approved by the Washington Editorial Review Board (WERB). Members of WERB are concerned not only with scientific merit but with three specific matters: measurement uncertainties, the use of SI units, and brand names. For more detailed information please <A href="https://www.ncnr.nist.gov/WERB.html">read more here</A> and/or speak with your NIST coauthor(s).</p>
+"""
+
+USANS_HEADER = r"""
+<p>If you have written a paper that reports measurements using the USANS Instrument and you do not see it in the list below, or if the paper is listed but the 
+information is incorrect or out of date, please inform <a href="mailto:jkrzywon@nist.gov">Jeff Krzywon</a> and/or <a href="mailto:barker@nist.gov">John Barker</a>.</p>
+<p>If you write a paper or give a presentation that reports measurements using the USANS Instrument, please be sure to:</p>
+<ol type="lower-alpha">
+<li>include an <A href="/programs/CHRNS/publications.html">acknowledgement</A> to the NSF, and to NIST if there are no NIST coauthors,and</li>
+<li>include a reference to the paper describing the USANS Instrument: "<a href="http://dx.doi.org/10.1107/S0021889805032103">Design and performance of a thermal-neutron double-crystal diffractometer for USANS at NIST</a>", Barker JG, et al. <i>J Appl. Cryst.</i> <b>38</b>(6), 1004 (2005).</li>
+</ol>
+<p>If you reduced your data using the Igor Macros, please include the reference ""<a href="http://dx.doi.org/10.1107/S0021889806035059">Reduction and Analysis of SANS and USANS Data using Igor Pro</a>", Kline, S. R. <i>J Appl. Cryst.</i> <b>39</b>(6), 895 (2006).</p>
+<p>Please be aware that all papers with NIST coauthors must be reviewed and approved by the Washington Editorial Review Board (WERB). Members of WERB are concerned not only with scientific merit but with three specific matters: measurement uncertainties, the use of SI units, and brand names. For more detailed information please <A href="https://www.ncnr.nist.gov/WERB.html">read more here</A> and/or speak with your NIST coauthor(s).</p>
+"""
+
+SANS_HEADER = r"""
+<p>If you have written a paper that reports measurements using any of the SANS instruments (NG3/NG7/NGB30 30m, NGB 10m, NG1 8m, BT5 USANS) and you do not see it in the list below, or if the paper is listed but the 
+information is incorrect or out of date, please inform <a href="mailto:jkrzywon@nist.gov">Jeff Krzywon</a> and/or <a href="mailto:butler@nist.gov">Paul Butler</a>.</p>
+<p>If you write a paper that reports measurements using any of the SANS Instruments, please be sure to:</p>
+<ol>
+<li>Include an acknowledgement to NIST if there are no NIST coauthors.</li>
+<li>For the NGB 30m SANS, USANS, and VSANS instruments include an <A href="/programs/CHRNS/publications.html"> acknowledgement</A> to the NSF.</li>
+<li>include a reference to the appropriate paper describing the Instrument used (not applicable for 8m SANS):</li>
+<ul>
+<li>NG7 and NGB 30m SANS: "<a href="http://dx.doi.org/10.1107/S0021889897017020">The 30 m Small-Angle Neutron Scattering Instruments at the National Institute of Standards and Technology</a>", Glinka CJ, et al. <i>J Appl. Cryst.</i> <b>31</b>(3), 430 (1998).</li>
+<li>USANS: "<a href="http://dx.doi.org/10.1107/S0021889805032103">Design and performance of a thermal-neutron double-crystal diffractometer for USANS at NIST</a>", Barker JG, et al. <i>J Appl. Cryst.</i> <b>38</b>(6), 1004 (2005).</li>
+</ul>
+</ol>
+<p>If you reduced your data using the Igor Macros, please include the reference ""<a href="http://dx.doi.org/10.1107/S0021889806035059">Reduction and Analysis of SANS and USANS Data using Igor Pro</a>", Kline, S. R. <i>J Appl. Cryst.</i> <b>39</b>(6), 895 (2006).</p>
+<p>Please be aware that all papers with NIST coauthors must be reviewed and approved by the Washington Editorial Review Board (WERB). Members of WERB are concerned not only with scientific merit but with three specific matters: measurement uncertainties, the use of SI units, and brand names. For more detailed information please <A href="https://www.ncnr.nist.gov/WERB.html">read more here</A> and/or speak with your NIST coauthor(s).</p>
 """
 
 SANS_FOOTER = r"""\
@@ -20,8 +70,8 @@ INSTRUMENTS = {
     },
     "NG7SANS": {
         "group": "1942669",
-        "collection": "RV37EK44", 
-        "header": SANS_HEADER,
+        "collection": "RV37EK44",
+        "header": NG7SANS_HEADER,
         "footer": SANS_FOOTER
     },
     "BT7": {
@@ -35,7 +85,9 @@ INSTRUMENTS = {
     },
     "NGB30SANS": {
         "group": "1942669",
-        "collection": "2TQMBS9X"
+        "collection": "2TQMBS9X",
+        "header": NGB30SANS_HEADER,
+        "footer": SANS_FOOTER
     },
     "NGB10SANS": {
         "group": "1942669",
@@ -45,9 +97,15 @@ INSTRUMENTS = {
         "group": "1942669",
         "collection": "XT6XYDEH"
     },
+    "SANS": {
+        "group": "1942669",
+        "collection": "BS3GD4WM"
+    },
     "USANS": {
         "group": "1942669",
-        "collection": "64AMFBBD"
+        "collection": "64AMFBBD",
+        "header": USANS_HEADER,
+        "footer": SANS_FOOTER
     },
     "DCS": {
         "group": "1942669",
