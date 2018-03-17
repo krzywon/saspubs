7f2c75c247aa819931fcda7a383d782c7c5842e3
bbm <brian.maranville@nist.gov>
2018-03-17 14:37:18 -0400

    add viewport meta tag for mobile viewing

--- citeproc_to_html.py
+++ citeproc_to_html.py
@@ -128,6 +128,7 @@ TEMPLATE = """\
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{title} Publications</title>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/drupal_header.css" />
