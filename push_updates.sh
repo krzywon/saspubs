4e1f02e80f30494b2aa3077609389ecf75cbeb8e
krzywon <krzywon@yahoo.com>
2019-04-16 10:38:18 -0400

    Update the bash script so ssh-agent does not continue to run in background.

--- push_updates.sh
+++ push_updates.sh
@@ -4,23 +4,20 @@
 # Pass a ssh private key file name as the first argument
 # saspubs and sasview.github.io must be on the same base path\
 
+# Get the SSH key from the arguments
+SSHKEY=$1
 # Pull latest saspubs repo
-git pull
+ssh-agent -t 60 bash -c "ssh-add $SSHKEY; git pull"
 # Output the latest publications page
 python instrument_zotero_feed.py
 # copy the page to the sasview.github.io repo
 cp static/SASVIEW_publications.md ../sasview.github.io/publications.md
 # Move to and pull latest sasview.github.io repo
 cd ../sasview.github.io/
-git pull
+ssh-agent -t 60 bash -c "ssh-add $SSHKEY; git pull"
 # Check for differences
 if git diff-index --quiet HEAD --; then
-  # Get the SSH key from the arguments
-  eval 'ssh-agent -s'
-  SSHKEY=$0
-  # Open the SSH channel
-  ssh-agent bash -c "ssh-add " + $SSHKEY
   # Commit and push any changes to the publications page
   git commit publications.md -m "publications auto-update `date -d now`"
-  git push
+  ssh-agent -t 60 bash -c "ssh-add $SSHKEY;  git push"
 fi
