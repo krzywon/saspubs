cccfd3ca47312d6f311317801ae1951bba96e1c7
krzywon <krzywon@yahoo.com>
2019-05-14 09:50:01 -0400

    Ensure bash script is looking at proper working directory.

--- push_updates.sh
+++ push_updates.sh
@@ -6,18 +6,20 @@
 
 # Get the SSH key from the arguments
 SSHKEY=$1
+# Change path to directory of the script in case it isn't already
+cd "$(dirname "$0")";
 # Pull latest saspubs repo
-ssh-agent -t 60 bash -c "ssh-add $SSHKEY; git pull"
+ssh-agent -t 60 bash -c "ssh-add $SSHKEY; git pull";
 # Output the latest publications page
-python instrument_zotero_feed.py
+python instrument_zotero_feed.py;
 # copy the page to the sasview.github.io repo
-cp static/SASVIEW_publications.md ../sasview.github.io/publications.md
+cp static/SASVIEW_publications.md ../sasview.github.io/publications.md;
 # Move to and pull latest sasview.github.io repo
-cd ../sasview.github.io/
-ssh-agent -t 60 bash -c "ssh-add $SSHKEY; git pull"
+cd ../sasview.github.io/;
+ssh-agent -t 60 bash -c "ssh-add $SSHKEY; git pull";
 # Check for differences
 if ! git diff-index --quiet HEAD --; then
   # Commit and push any changes to the publications page
-  git commit publications.md -m "publications auto-update `date -d now`"
-  ssh-agent -t 60 bash -c "ssh-add $SSHKEY;  git push"
+  git commit publications.md -m "publications auto-update `date -d now`";
+  ssh-agent -t 60 bash -c "ssh-add $SSHKEY;  git push";
 fi
